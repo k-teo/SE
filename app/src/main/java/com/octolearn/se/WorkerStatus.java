e9984d3d4e95f33c2c48cfef95f68cf98f07b451
MKDMK <dams.michal@onet.pl>
2021-01-04 22:30:11 +0100

    added all necessary classes

---
+++ app/src/main/java/com/octolearn/se/WorkerStatus.java
@@ -0,0 +1,14 @@
+package com.octolearn.se;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkerStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_status);
+    }
+}
